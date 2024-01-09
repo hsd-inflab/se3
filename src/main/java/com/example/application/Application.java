0a85909d5f845dc3dd1e696fe54932e73739ba47
MoritzKlerkx <moritzklerkx@hotmail.de>
2024-01-09 09:55:31 +0100

    bulb

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
+
 @Theme(value = "mytodo")
 public class Application implements AppShellConfigurator {
 
