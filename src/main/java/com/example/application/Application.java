342cf2c8b10f7681d2eafe63a610140185bcb785
MoritzKlerkx <moritzklerkx@hotmail.de>
2024-01-09 09:54:20 +0100

    NewLine

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 @Theme(value = "mytodo")
 public class Application implements AppShellConfigurator {
 
